3281d55c8afbc00ce4b915586fa943bc1b045933
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-09-16 17:43:34 +0300

    setup MySQL database connection

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import db from './config/db.js';
 
-dotenv.config();
+dotenv.config({quiet: true});
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server!');
-}); 
+    res.status(200).send('API is running');
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

0612b40c96af82a6060678682582128f50085851
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-09-16 22:52:33 +0300

    setup Sequelize connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import db from './config/db.js';
+import sequelize from './config/db.js';
 
 dotenv.config({quiet: true});
 
 const app = express();
 app.use(express.json());
+
+try {
+    await sequelize.authenticate();
+    console.log('Database successfully connected!')
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 app.get('/', (req, res) => {
     res.status(200).send('API is running');
--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,21 @@
-import mysql from 'mysql2/promise'; 
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const db = mysql.createPool({
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    dialect: 'mysql',
+    logging: false
 });
 
-export default db;
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+
+export default sequelize;
 

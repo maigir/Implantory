a77b7d17ca4fd41390e6480a86bb7a5677a4c7ba
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-04 22:43:54 +0300

    Add BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/login' element={<Login />} />
-      <Route 
-        path='/dashboard' 
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } 
-      />
-      <Route path='*' element={<Navigate to='/login' />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route 
+          path='/dashboard' 
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+        <Route path='*' element={<Navigate to='/login' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

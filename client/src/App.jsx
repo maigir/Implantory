5b829737af6080fce37bfc65c7a7b1109a311549
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-05 12:48:03 +0300

    Delete <BrowserRouter /> from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route 
-          path='/dashboard' 
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          } 
-        />
-        <Route path='*' element={<Navigate to='/login' />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route 
+        path='/dashboard' 
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        } 
+      />
+      <Route path='*' element={<Navigate to='/login' />} />
+    </Routes>
   );
 }
 

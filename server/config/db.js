5c1cb9f700e5d4c2dd67580022c994c34d51658e
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-05 15:23:39 +0300

    Change ssl in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     dialectOptions: {
         ssl: {
-            rejectUnauthorized: true,
+            rejectUnauthorized: false,
         },
     },
     logging: false

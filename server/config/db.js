301ebb4696c009b239f5ffe9192cad68c7eb73a4
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-05 15:36:43 +0300

    Add log to db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 dotenv.config();
-
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     dialectOptions: {

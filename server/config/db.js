d90be6c0e7d00a3fa0823f7ef95884a0afb2e482
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-05 16:00:53 +0300

    Add .env variables separately

--- server/config/db.js
+++ server/config/db.js
@@ -2,16 +2,23 @@ import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 dotenv.config();
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false,
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {   
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
         },
-    },
-    logging: false
-});
+        logging: false
+    }
+);
 
 export default sequelize;
 

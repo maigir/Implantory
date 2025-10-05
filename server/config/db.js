b08fa47d724e98bee76b9df56fbef674cf0b2b60
Maigi Rannamets <rannamets.maigi@gmail.com>
2025-10-05 13:32:44 +0300

    Renew backend db.js config

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,7 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     dialectOptions: {
         ssl: {
